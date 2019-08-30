3ecd3acf98742015337eea204266a5dddaac7498
Levi Borland <borland.levi@gmail.com>
2019-08-30 16:19:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 class Shoe
   
-  attr_accessor :color, :size, :material, :condition
-  
   attr_reader :brand
   
-  def initialize(brand)
+  def initialize(brand, color = "red")
     @brand = brand
   end
+  
+  
   
   def cobble
     puts "the shoe has been repaired"
